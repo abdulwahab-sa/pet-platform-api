ba69031a63daf43df69d47bbe9cf9190fa85c399
Abdul Wahab <aws18320@gmail.com>
2023-10-23 16:02:10 +0500

    Refactored Cors

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
 const cookieParser = require('cookie-parser');
 
 const corsOptions = {
-	origin: '*',
+	origin: 'https://pet-platform.netlify.app',
 	credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Origin', 'https://pet-platform.netlify.app');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
 	next();
